381ffa73eb62d9ac7bb96de689d9a1a321781993
FMPASS <gugusenlin@gmail.com>
2024-06-16 06:19:35 +0800

    Update face.js

--- chipper/face.js
+++ chipper/face.js
@@ -1,5 +1,18 @@
 var body = $response.body;
-var obj = JSON.parse(body);
-obj['isFrozen'] = false
-body = JSON.stringify(obj);
-$done({body});
+let url = $request.url;
+const regex_rul_statues = /^https:\/\/auth\.chippercash\.com\/session\/status/;
+const regex_rul_unfreeze = /^https:\/\/auth\.chippercash\.com\/session\/unfreeze/;
+
+if (regex_rul_statues.test(url)) {
+    var obj = JSON.parse(body);
+    obj['isFrozen'] = false
+    body = JSON.stringify(obj);
+    $done({body});
+} else if (regex_rul_unfreeze.test(url)) {
+    var obj = JSON.parse(body);
+    obj['success'] = true
+    body = JSON.stringify(obj);
+    $done({body});
+} else {
+    $done({body});
+}
